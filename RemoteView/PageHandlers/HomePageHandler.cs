15ec99d3521c7981704c02ec49b6cf538de6e53b
vilaca <jvilaca@gmail.com>
2013-06-08 00:30:26 +0100

    (add) comments

--- RemoteView/PageHandlers/HomePageHandler.cs
+++ RemoteView/PageHandlers/HomePageHandler.cs
@@ -4,29 +4,45 @@ using System.Windows.Forms;
 
 namespace RemoteView.PageHandlers
 {
+    /// <summary>
+    /// This class is responsable for generaring all the HTML and javascript the the application homepage
+    /// </summary>
     class HomePageHandler : PageHandler
     {
+        // screen devices list
+
         private Screen[] screens = Screen.AllScreens;
 
         public override byte[] handleRequest(HttpListenerResponse response, String[] uri)
         {
-            String staticPage = "<html><title>Desktop view</title><body><p>";
 
+            // basic html, including head tags
+            
+            String staticPage = "<html><head><title>Desktop view</title></head><body>";
+            
+            // list amount of screen devices
+
+            staticPage +="<p>";
             for (int i = 0; i < screens.Length; i++)
             {
                 staticPage += "| <a href=\"/home/" + i + "\">Screen:" + i + "</a>";
             }
-
             staticPage += "</p>";
+
+            // image for current screen device
 
             string screen = uri.Length > 2 ? "/" + uri[2] : "/0";
             staticPage += "<img id=\"image\" name=\"image\" src=\"/screen" + screen + "\">";
+
+            // script for handling clicks/dblclicks
 
             staticPage += "<script>" +
                 "image.addEventListener('contextmenu', function(e){ handleInput('r',e); });" +
                 "image.addEventListener('click', function(e){ handleInput('l',e); });" +
                 "function handleInput(c, e) { e.stopPropagation();e.preventDefault(); }" +
                 "</script>";
+
+            // closing body and html tags
 
             staticPage += "</body></html>";
 

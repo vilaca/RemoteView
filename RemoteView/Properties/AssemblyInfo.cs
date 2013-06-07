f7fe5cef3629b6180c6a34a67820dfb4a295de39
vilaca <jvilaca@gmail.com>
2013-06-07 03:15:57 +0200

    Update AssemblyInfo.cs

--- RemoteView/Properties/AssemblyInfo.cs
+++ RemoteView/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RemoteView")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Desktop sharing application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("vilaca")]
 [assembly: AssemblyProduct("RemoteView")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]

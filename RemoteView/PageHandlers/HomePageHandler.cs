837a5e4bcd20607396d1101d7fe3b643c44986ea
vilaca <jvilaca@gmail.com>
2013-06-08 00:49:22 +0100

    (add) page /click/ is now called for handling clicks (fix) more comments

--- RemoteView/PageHandlers/HomePageHandler.cs
+++ RemoteView/PageHandlers/HomePageHandler.cs
@@ -4,42 +4,66 @@ using System.Windows.Forms;
 
 namespace RemoteView.PageHandlers
 {
-    /// <summary>
-    /// This class is responsable for generaring all the HTML and javascript the the application homepage
-    /// </summary>
+    // This class is responsable for generaring all the HTML and javascript the the application homepage
+
     class HomePageHandler : PageHandler
     {
         // screen devices list
 
         private Screen[] screens = Screen.AllScreens;
 
+        /// <summary>
+        /// Generate the HTML and javascript needed for this the homepage
+        /// </summary>
+        /// <param name="response">not used</param>
+        /// <param name="uri">raw URI tokenized by '/'</param>
+        /// <returns>HTML page + javascript</returns>
         public override byte[] handleRequest(HttpListenerResponse response, String[] uri)
         {
 
             // basic html, including head tags
-            
+
             String staticPage = "<html><head><title>Desktop view</title></head><body>";
-            
+
             // list amount of screen devices
 
-            staticPage +="<p>";
+            staticPage += "<p>";
             for (int i = 0; i < screens.Length; i++)
             {
                 staticPage += "| <a href=\"/home/" + i + "\">Screen:" + i + "</a>";
             }
             staticPage += "</p>";
 
-            // image for current screen device
+            // display current screen on image
+            // if no parameter for screen, default to 0
+            int screen = 0;
+            if (uri.Length > 2)
+            {
+                try
+                {
+                    screen = Convert.ToInt16(uri[2]);
+                }
+                catch {}
 
-            string screen = uri.Length > 2 ? "/" + uri[2] : "/0";
-            staticPage += "<img id=\"image\" name=\"image\" src=\"/screen" + screen + "\">";
+                if ( screen < 0 || screen >= screens.Length )
+                {
+                    screen = 0;
+                }
+            }
+            staticPage += "<img id=\"image\" name=\"image\" src=\"/screen/" + screen + "\">";
 
             // script for handling clicks/dblclicks
-
+            // page /script/ is called for handling clicks
             staticPage += "<script>" +
                 "image.addEventListener('contextmenu', function(e){ handleInput('r',e); });" +
                 "image.addEventListener('click', function(e){ handleInput('l',e); });" +
-                "function handleInput(c, e) { e.stopPropagation();e.preventDefault(); }" +
+                "function handleInput(c, e) { " +
+                "e.stopPropagation();" +
+                "e.preventDefault();" +
+                "px = e.offsetX ? e.offsetX :e.pageX-document.getElementById(\"image\").offsetLeft;" +
+                "py = e.offsetY ? e.offsetY :e.pageY-document.getElementById(\"image\").offsetTop;" +
+                "window.location='/click/" + screen + "/' + c + '/' + py + '/' + px;"
+                + "}" +
                 "</script>";
 
             // closing body and html tags

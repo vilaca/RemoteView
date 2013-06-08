7ce4d905be251751cee70f83eb23274e7b7a02ee
vilaca <jvilaca@gmail.com>
2013-06-08 01:10:35 +0100

    (add) added shared function in abstract class PageHandler

--- RemoteView/PageHandler.cs
+++ RemoteView/PageHandler.cs
@@ -1,11 +1,31 @@
 ﻿
 using System;
 using System.Net;
+using System.Windows.Forms;
+
 namespace RemoteView
 {
     abstract class PageHandler
     {
         public abstract byte[] handleRequest(HttpListenerResponse response, String[] uri);
 
+        internal int getRequestedScreenDevice(String[] uri, Screen[] screens)
+        {
+            int screen = 0;
+            if (uri.Length > 2)
+            {
+                try
+                {
+                    screen = Convert.ToInt16(uri[2]);
+                }
+                catch { }
+
+                if (screen < 0 || screen >= screens.Length)
+                {
+                    screen = 0;
+                }
+            }
+            return screen;
+        }
     }
 }

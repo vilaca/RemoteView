0f0debd542f80ff01038384faa93f11d95d36cc2
vilaca <jvilaca@gmail.com>
2013-06-14 20:24:49 +0100

    (add) buffer icon in IconPageHandler.cs

--- RemoteView/PageHandlers/IconPageHandler.cs
+++ RemoteView/PageHandlers/IconPageHandler.cs
@@ -8,14 +8,21 @@ namespace RemoteView.PageHandlers
 {
     class IconPageHandler : PageHandler
     {
-        public override byte[] handleRequest(HttpListenerResponse response, string[] uri)
+        byte[] buffer;
+
+        public IconPageHandler()
         {
             using (MemoryStream icon = new MemoryStream())
             {
                 Icon bitmap = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
                 bitmap.Save(icon);
-                return icon.GetBuffer();
+                buffer = icon.GetBuffer();
             }
+        }
+
+        public override byte[] handleRequest(HttpListenerResponse response, string[] uri)
+        {
+            return buffer;
         }
     }
 }

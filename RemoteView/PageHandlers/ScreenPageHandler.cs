07b822f6c0e5a98a5829590e3b07a9c06219b28d
vilaca <jvilaca@gmail.com>
2013-06-20 21:45:39 +0100

    (add) offload image creation from ScreenPageHandler handleRquest()

--- RemoteView/PageHandlers/ScreenPageHandler.cs
+++ RemoteView/PageHandlers/ScreenPageHandler.cs
@@ -10,19 +10,50 @@ namespace RemoteView.PageHandlers
     class ScreenPageHandler : AbstractPageHandler
     {
         private Screen[] screens = Screen.AllScreens;
+        private volatile byte[][] caches = new byte[Screen.AllScreens.Length][];
+        volatile int lastScreenRequested = 0;
+        private System.Timers.Timer timer;
+
+        public ScreenPageHandler()
+        {
+            for (int i = 0; i < caches.Length; i++)
+            {
+                caches[i] = serializeScreenImage(i);
+            }
+
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += timer_Elapsed;
+            timer.Enabled = true;
+        }
+
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            int toReload = lastScreenRequested;
+            caches[toReload] = serializeScreenImage(toReload);
+        }
 
         /// <summary>
-        /// Get a copy of the requested device screen image
+        /// Get a copy of the requested device screen image from cache
         /// </summary>
         /// <param name="response"></param>
         /// <param name="uri"></param>
         /// <returns></returns>
         public override byte[] handleRequest(HttpListenerResponse response, String[] uri)
         {
+            int requested = getRequestedScreenDevice(uri, screens);
+            lastScreenRequested = requested;
             response.Headers.Set("Content-Type", "image/png");
+            return caches[requested];
+        }
 
-            Screen screen = screens[getRequestedScreenDevice(uri, screens)];
-
+        /// <summary>
+        /// Serizalize a screen
+        /// </summary>
+        /// <param name="requested">screen to serialize</param>
+        /// <returns></returns>
+        private byte[] serializeScreenImage(int requested)
+        {
+            Screen screen = screens[requested];
             using (MemoryStream ms = new MemoryStream())
             {
                 Bitmap bmp = new Bitmap(screen.Bounds.Width, screen.Bounds.Height);
@@ -31,5 +62,6 @@ namespace RemoteView.PageHandlers
                 return ms.GetBuffer();
             }
         }
+
     }
 }
